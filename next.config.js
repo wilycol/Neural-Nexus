801dac34c4f1ff456accbec84caf5f28a82ad3d2
Wily Col <wilycol1492@gmail.com>
2026-03-28 15:55:42 -0500

    fix: remove custom distDir to comply with Vercel default output path

--- next.config.js
+++ next.config.js
@@ -26,7 +26,6 @@ const baseConfig = {
 };
 
 module.exports = (phase) => {
-  const distDir = phase === PHASE_DEVELOPMENT_SERVER ? ".next-dev" : ".next-build";
-  return { ...baseConfig, distDir };
+  return { ...baseConfig };
 };
 // Force deploy trigger - 03/28/2026 15:47:18
