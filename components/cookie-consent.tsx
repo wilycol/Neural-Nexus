7364b84fcdf4bacca2e6730382e68b76f32afbd0
Wily Col <wilycol1492@gmail.com>
2026-04-15 17:51:07 -0500

    Update cookie-consent.tsx

--- components/cookie-consent.tsx
+++ components/cookie-consent.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect, useState } from "react";
 import { X, Cookie, ShieldCheck } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
 export function CookieConsent() {
   const [isVisible, setIsVisible] = useState(false);
