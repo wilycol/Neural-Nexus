ad4cfc7ca22eee0c6e919e9c8c9c6548e078fa9e
Wily Col <wilycol1492@gmail.com>
2026-05-13 08:11:31 -0500

    Industrialization: Level 6 PWA Deployment

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'nexus-command-v1';
+const ASSETS = [
+  '/command.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
