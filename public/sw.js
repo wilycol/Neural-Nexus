e3485bcb2f2add101bb951cbb8e4addb5ef0130e
Wily Col <wilycol1492@gmail.com>
2026-05-15 07:31:20 -0500

    Fix Voice/Image rendering in PWA (Outbox Poll)

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,24 @@
-const CACHE_NAME = 'nexus-command-v2.9';
+const CACHE_NAME = 'nexus-command-v3.0';
 const ASSETS = [
+  '/',
   '/command.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+  )));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
