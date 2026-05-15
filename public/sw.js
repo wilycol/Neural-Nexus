448cda2f71ad8e038133b36a1d6e0c6cf50669c2
Wily Col <wilycol1492@gmail.com>
2026-05-15 09:38:26 -0500

    Industrialization v3.8.0: Sync Precision & Force Refresh

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'nexus-command-v3.7.0';
+const CACHE_NAME = 'nexus-command-v3.8.0';
 const ASSETS = [
   '/',
-  '/command.html',
+  '/command.html?v=3.8.0',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
@@ -11,12 +11,14 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(caches.keys().then(keys => Promise.all(
     keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
   )));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
