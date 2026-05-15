74a513215d569010f6a29df91ca6d5c114d11c87
Wily Col <wilycol1492@gmail.com>
2026-05-15 10:26:48 -0500

    Nexus v4.1: Critical Rendering Fix & Bubble Restoration

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'nexus-command-v4.0.0';
+const CACHE_NAME = 'nexus-command-v4.1.0';
 const ASSETS = [
   '/',
-  '/command.html?v=4.0.0',
+  '/command.html?v=4.1.0',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
