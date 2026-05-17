e47250a21de7702af356380dadc1edee0c68e302
Wily Col <wilycol1492@gmail.com>
2026-05-17 07:09:25 -0500

    feat: integrate industrial Web Push & VAPID (FCM) support

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'nexus-command-v4.4.0';
+const CACHE_NAME = 'nexus-command-v4.5.0';
 const ASSETS = [
   '/',
-  '/command.html?v=4.4.0',
+  '/command.html?v=4.5.0',
   '/manifest.json',
   '/icons/icon-192.png',
   '/icons/icon-512.png',
